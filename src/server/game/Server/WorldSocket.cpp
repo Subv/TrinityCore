2c85e239789689e5eea8b4d43e7836b90a4b82e8
Subv <s.v.h21@hotmail.com>
2012-07-13 11:43:36 -0500

    Fixed SMSG_AUTH_CHALLENGE

--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -244,17 +244,18 @@ int WorldSocket::open (void *a)
 
     // Send startup packet.
     WorldPacket packet (SMSG_AUTH_CHALLENGE, 24);
-    packet << uint32(1);                                    // 1...31
-    packet << m_Seed;
-
+    
     BigNumber seed1;
     seed1.SetRand(16 * 8);
     packet.append(seed1.AsByteArray(16), 16);               // new encryption seeds
+    
+    packet << uint8(1);                                    // 1...31
+    packet << uint32(m_Seed);
 
     BigNumber seed2;
     seed2.SetRand(16 * 8);
     packet.append(seed2.AsByteArray(16), 16);               // new encryption seeds
-
+    
     if (SendPacket(packet) == -1)
         return -1;
 
